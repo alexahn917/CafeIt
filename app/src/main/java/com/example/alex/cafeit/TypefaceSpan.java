d1fcc558d3b06750054eddbc6883091133252e1a
Alex Ahn <alexahn917@gmail.com>
2017-04-20 21:41:00 -0400

    changed back font size

--- app/src/main/java/com/example/alex/cafeit/TypefaceSpan.java
+++ app/src/main/java/com/example/alex/cafeit/TypefaceSpan.java
@@ -38,10 +38,9 @@ public class TypefaceSpan extends MetricAffectingSpan {
     @Override
     public void updateMeasureState(TextPaint p) {
         p.setTypeface(mTypeface);
-
         // Note: This flag is required for proper typeface rendering
         p.setFlags(p.getFlags() | Paint.SUBPIXEL_TEXT_FLAG);
-        p.setTextSize(120);
+        p.setTextSize(80);
     }
 
     @Override
@@ -49,6 +48,6 @@ public class TypefaceSpan extends MetricAffectingSpan {
         tp.setTypeface(mTypeface);
         // Note: This flag is required for proper typeface rendering
         tp.setFlags(tp.getFlags() | Paint.SUBPIXEL_TEXT_FLAG);
-        tp.setTextSize(120);
+        tp.setTextSize(80);
     }
 }
