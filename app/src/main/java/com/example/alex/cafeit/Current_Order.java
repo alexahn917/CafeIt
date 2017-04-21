71aa2439abd34c1759fb4c1741b392497aa624f1
Alex Ahn <alexahn917@gmail.com>
2017-04-20 22:57:57 -0400

    fixed current order view

--- app/src/main/java/com/example/alex/cafeit/Current_Order.java
+++ app/src/main/java/com/example/alex/cafeit/Current_Order.java
@@ -15,8 +15,7 @@ import android.widget.Toast;
 
 public class Current_Order extends AppCompatActivity {
     // Remove the below line after defining your own ad unit ID.
-    private static final String TOAST_TEXT = "Test ads are being shown. "
-            + "To show live ads, replace the ad unit ID in res/values/strings.xml with your own ad unit ID.";
+    private static final String TOAST_TEXT = "Test ads coming up.";
 
     private static final int START_LEVEL = 1;
     private int mLevel;
