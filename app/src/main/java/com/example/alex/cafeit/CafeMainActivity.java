f583f184242eb1f0584a980bcb55833c66c83834
Anthony Kim <anthonysunghoonkim@gmail.com>
2017-04-21 14:10:33 -0400

    Barista side UI: "Save" floating action button for cafe menu

--- app/src/main/java/com/example/alex/cafeit/CafeMainActivity.java
+++ app/src/main/java/com/example/alex/cafeit/CafeMainActivity.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 //import android.app.Fragment;
 import android.os.Bundle;
@@ -19,7 +21,9 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CafeMainActivity extends AppCompatActivity {
 
@@ -29,9 +33,9 @@ public class CafeMainActivity extends AppCompatActivity {
 
     private Fragment profileFragment = new CafeProfileFragment();
     private Fragment ordersFragment = new OrdersFragment();
-
     private Fragment menuFragment = new CafeMenuFragment();
 
+    private FloatingActionButton saveFab;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -42,14 +46,17 @@ public class CafeMainActivity extends AppCompatActivity {
                 case R.id.navigation_orders:
                     setTitle("Orders");
                     getSupportFragmentManager().beginTransaction().replace(R.id.content_cafe, ordersFragment).commit();
+                    saveFab.setVisibility(View.INVISIBLE);
                     return true;
                 case R.id.navigation_menu:
                     getSupportFragmentManager().beginTransaction().replace(R.id.content_cafe, menuFragment).commit();
-                    setTitle("Menu");
+                    setTitle("Menu Listing");
+                    saveFab.setVisibility(View.VISIBLE);
                     return true;
                 case R.id.navigation_cafe_profile:
                     getSupportFragmentManager().beginTransaction().replace(R.id.content_cafe, profileFragment).commit();
                     setTitle("Profile");
+                    saveFab.setVisibility(View.INVISIBLE);
                     return true;
             }
             return false;
@@ -64,6 +71,14 @@ public class CafeMainActivity extends AppCompatActivity {
         context = getApplicationContext();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        saveFab = (FloatingActionButton) findViewById(R.id.saveFAB);
+        saveFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Menu saved!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation_cafe_layout);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
