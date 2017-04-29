1530ddce5a282bebf41ffdada8a7f71b989426c5
Jin Young Sohn <jysohn23@gmail.com>
2017-04-29 17:23:28 -0400

    sorting

---
+++ app/src/main/java/com/example/alex/cafeit/Sorter.java
@@ -0,0 +1,20 @@
+package com.example.alex.cafeit;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.example.alex.cafeit.Cafe;
+
+
+/**
+ * Created by jinyoungsohn on 4/29/17.
+ */
+
+public final class Sorter {
+
+    private Sorter() {
+    }
+
+    public List<Cafe> byDistance(List<Cafe> cafeList) {
+        return new ArrayList<>();
+    }
+}
