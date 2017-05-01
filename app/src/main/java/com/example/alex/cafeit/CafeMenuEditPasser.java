48e35a56d1eec254960a520b25672aa678d2b725
Anthony Kim <anthonysunghoonkim@gmail.com>
2017-05-01 02:39:34 -0400

    barista side menu editing fully functional

---
+++ app/src/main/java/com/example/alex/cafeit/CafeMenuEditPasser.java
@@ -0,0 +1,14 @@
+package com.example.alex.cafeit;
+
+/**
+ * Created by Anthony Kim on 5/1/2017.
+ */
+
+public class CafeMenuEditPasser {
+    public static CafeMenuItem item;
+
+    public CafeMenuEditPasser(){
+
+    }
+
+}
